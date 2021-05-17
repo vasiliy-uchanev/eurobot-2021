027a2570cd24f31142a355a80bfe911375f525d7
vasiliy.uchanev <vasiliy.uchanev@servicetitan.com>
2021-05-17 18:27:50 +0500

    initial code that makes motor rotate on interrupt from the mothership

---
+++ swinger/swinger.ino
@@ -0,0 +1,71 @@
+#define interrruptDropPin 2
+#define interruptGrabPin 3
+#define dirPin 4
+#define stepPin 5
+#define testHighPin 6
+
+#define stepsDownCount 4000
+#define stepsUpCount 1000
+#define delayMicroSecs 500
+
+enum swingerMode {
+  none,
+  grab,
+  drop
+};
+
+swingerMode mode = none;
+
+void setup() {
+  pinMode(stepPin, OUTPUT);
+  pinMode(dirPin, OUTPUT);
+  pinMode(testHighPin, OUTPUT);
+  digitalWrite(testHighPin, HIGH);
+  attachInterrupt(digitalPinToInterrupt(interrruptDropPin), handleDropCall, RISING);
+  attachInterrupt(digitalPinToInterrupt(interruptGrabPin), handleGrabCall, RISING);
+}
+
+void loop() {
+  if(mode == grab) {
+    performDownUpCycle();
+    mode = none;
+  }
+  if(mode == drop) {
+    performUpDownCycle();
+    mode = none;
+  }
+}
+
+void handleGrabCall() {
+  mode = grab;
+}
+
+void handleDropCall() {
+  mode = drop;
+}
+
+
+void performDownUpCycle() {
+  digitalWrite(dirPin, HIGH);
+  spin(stepsUpCount);
+  delay(100);
+  digitalWrite(dirPin, LOW);
+  spin(stepsUpCount);
+}
+
+void performUpDownCycle() {
+  digitalWrite(dirPin, LOW);
+  spin(stepsDownCount);
+  delay(100);
+  digitalWrite(dirPin, HIGH);
+  spin(stepsDownCount);
+}
+
+void spin(int steps) {
+  for (int i = 0; i < steps; i++) {
+    digitalWrite(stepPin, HIGH);
+    delayMicroseconds(delayMicroSecs);
+    digitalWrite(stepPin, LOW);
+    delayMicroseconds(delayMicroSecs);
+  }
+}
